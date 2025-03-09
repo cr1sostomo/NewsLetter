a29fac2f5780878cc44c3fc00141850f4683404b
cr1sostomo <lucascrisostomo9120@gmail.com>
2025-03-09 13:20:42 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Components/NewsLetter.css'
+import NewsLetter from './Components/NewsLetter.jsx'
+
+function App() {
+  return (
+    <>
+    <div>
+      <NewsLetter/>
+    </div>
+    </>
+  )
+}
+
+export default App
